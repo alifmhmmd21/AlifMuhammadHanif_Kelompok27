deabcbb2c914efd8534c6201c44f497d3cc74d9e
alifmhmmd21 <57091089+alifmhmmd21@users.noreply.github.com>
2022-03-25 14:15:04 +0700

    Add files via upload

---
+++ minyakgoreng.java
@@ -0,0 +1,13 @@
+package buj;
+
+public class minyakgoreng {
+    static void minyak(){
+        System.out.println(" Anda Memilih Minyak Goreng : Rp 24.000 ");
+        return;
+    }
+    static int goreng(int a){
+        int hasil = a *24 +3;
+        return hasil;
+
+    }
+}
