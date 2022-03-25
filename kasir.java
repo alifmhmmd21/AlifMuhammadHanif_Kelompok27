e6fde474fc39084f814ca5f7e79086eae64ea348
alifmhmmd21 <57091089+alifmhmmd21@users.noreply.github.com>
2022-03-25 14:14:39 +0700

    Add files via upload

---
+++ kasir.java
@@ -0,0 +1,11 @@
+package buj;
+
+public class kasir {
+    static void welcom(){
+        System.out.println("Selamat Datang di Sistem Kasir");
+    }
+    static String welcom1(){
+        return "Silahkan memilih menu yang tertera ";
+    }
+    
+}
