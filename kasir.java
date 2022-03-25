6136d6451223a89dace17aa3b20281fd1f81a5c2
alifmhmmd21 <57091089+alifmhmmd21@users.noreply.github.com>
2022-03-25 14:22:49 +0700

    Update kasir.java

--- kasir.java
+++ kasir.java
@@ -7,5 +7,12 @@ public class kasir {
     static String welcom1(){
         return "Silahkan memilih menu yang tertera ";
     }
-    
+    public static void main(String[] args){
+        System.out.println(" ");
+        welcom();
+        System.out.println(welcom1());
+        minyakgoreng mgr = new minyakgoreng();
+        mgr.minyak();
+
+    }
 }
