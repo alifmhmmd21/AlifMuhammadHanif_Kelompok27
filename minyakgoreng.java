9b6314f56eadb576aca4a0105eec106eeb10628d
alifmhmmd21 <57091089+alifmhmmd21@users.noreply.github.com>
2022-03-26 21:01:00 +0700

    Update minyakgoreng.java

--- minyakgoreng.java
+++ minyakgoreng.java
@@ -1,10 +1,34 @@
+package buj;
+
 public class minyakgoreng {
     static void minyak(String barang, int harga) {
         System.out.println("Kamu memilih Barang " + barang + " dengan harga : Rp" + harga + "000");
     }
 
     static int goreng(int a){
-        int hasil =  a+24;
+        return (a) + 24 ;
+    }
+    static void roti(String barang, int harga){
+        System.out.println("Kamu memilih Barang " + barang + " dengan harga : Rp" + harga + "000");
+    }
+
+    static int tawar(int a){
+        return (a) + 15;
+    }
+    static void pasta(String barang, int harga) {
+        System.out.println("Kamu memilih Barang " + barang + " dengan harga : Rp" + harga + "000");
+    }
+
+    static int gigi(int a){
+        int hasil =  (a) + 12;
+        return hasil;
+    }
+    static void saos(String barang, int harga) {
+        System.out.println("Kamu memilih Barang " + barang + " dengan harga : Rp" + harga + "000");
+    }
+
+    static int tiram(int a){
+        int hasil =  (a) + 7;
         return hasil;
     }
 }
