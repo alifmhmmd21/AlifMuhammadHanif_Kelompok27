b49146157466719b0d217c69f4a38bbc85ad53b3
alifmhmmd21 <57091089+alifmhmmd21@users.noreply.github.com>
2022-03-26 13:28:41 +0700

    Update kasir.java

--- kasir.java
+++ kasir.java
@@ -28,7 +28,7 @@ public class kasir {
         System.out.println(" ");
         System.out.println("========= List Barang ========");
         System.out.println("[1] Minyak Goreng");
-        System.out.println("[2] ?");
+        System.out.println("[2] Roti Tawar");
         System.out.println("[3] ?");
         System.out.println("[4] ?");
         System.out.print("PILIH MENU> ");
@@ -43,7 +43,14 @@ public class kasir {
                 int pajak = mgr.goreng(3);
                 int total = pilihan * pajak;
                 System.out.println("Totalnya: Rp"+ total + "000");
-
+                break;
+            case 2:
+                mgr.roti("Roti Tawar", 15);
+                System.out.print("Berapa banyak barang yang adan inginkan: ");
+                int pilihan1 = sc.nextInt();
+                int pajak1 = mgr.tawar(1);
+                int total1 = pilihan1 * pajak1;
+                System.out.println("Totalnya: Rp"+ total1 + "000");
                 break;
             case 4:
                 System.exit(0);
