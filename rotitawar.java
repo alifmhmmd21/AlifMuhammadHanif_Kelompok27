15c0ff26a3e5605fb5fc9d5d9c622cb7c3b6b1e3
DarrylKresna <102150312+DarrylKresna@users.noreply.github.com>
2022-03-26 12:11:22 +0700

    Create rotitawar.java

---
+++ rotitawar.java
@@ -0,0 +1,10 @@
+public class rotitawar {
+  static void roti(){
+    System.out.println(" Anda memilih Roti Tawar : Rp 15.000 ");
+    return;
+  }
+  static int tawar(int a){
+    int hasil = a *15 +3;
+    return hasil;
+  }
+}
