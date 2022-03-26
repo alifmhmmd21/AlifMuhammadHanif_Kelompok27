a4e577713d56a710dbc73f6cd5d4288e9e796000
zaqiayuna <102151016+zaqiayuna@users.noreply.github.com>
2022-03-26 18:42:36 +0700

    Create saos tiram.java

---
+++ saos tiram.java
@@ -0,0 +1,10 @@
+public class minyakgoreng {
+    static void saos(String barang, int harga) {
+        System.out.println("Kamu memilih Barang " + barang + " dengan harga : Rp" + harga + "000");
+    }
+
+    static int tiram(int a){
+        int hasil =  a+7;
+        return hasil;
+    }
+}
